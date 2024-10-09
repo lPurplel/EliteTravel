ac5c5cba98ed2d53102e49381b3a26d8b5b870b2
erdaulet <erdauletakilzhan83@gmail.com>
2024-10-10 00:21:45 +0500

    2-2 added, fixed pages

--- MyProject/MyApp/urls.py
+++ MyProject/MyApp/urls.py
@@ -3,7 +3,17 @@ from . import views
 
 urlpatterns=[
     path('', views.home, name="home"),
-    path("/jets", views.jets, name="jets"),
-    path("/about_us", views.about_us, name="about_us"),
-    path("/booked_flights", views.booked_flights, name="booked_flights")
+    path("jets", views.jets, name="jets"),
+    path("aboutUs", views.aboutUs, name="aboutUs"),
+    path("booked", views.booked, name="booked"),
+    path("booking", views.booking, name="booking"),
+    path("light", views.light, name="light"),
+    path("light1", views.light1, name="light1"),
+    path("light2", views.light2, name="light2"),
+    path("heavy", views.heavy, name="heavy"),
+    path("heavy1", views.heavy1, name="heavy1"),
+    path("heavy2", views.heavy2, name="heavy2"),
+    path("airline", views.airline, name="airline"),
+    path("airline1", views.airline1, name="airline1"),
+    path("airline2", views.airline2, name="airline2")
     ]
--- MyProject/MyApp/views.py
+++ MyProject/MyApp/views.py
@@ -8,8 +8,41 @@ def home(request):
 def jets(request):
     return render(request, "jets.html")
 
-def about_us(request):
-    return render(request, "about_us.html")
+def aboutUs(request):
+    return render(request, "aboutUs.html")
 
-def booked_flights(request):
-    return render(request, "booked_flights.html")
+def booked(request):
+    return render(request, "booked.html")
+
+def light(request):
+    return render(request, "light.html")
+
+def light1(request):
+    return render(request, "light1.html")
+
+def light2(request):
+    return render(request, "light2.html")
+
+def heavy(request):
+    return render(request, "heavy.html")
+
+def heavy1(request):
+    return render(request, "heavy1.html")
+
+def heavy2(request):
+    return render(request, "heavy2.html")
+
+def airline(request):
+    return render(request, "airline.html")
+
+def airline1(request):
+    return render(request, "airline1.html")
+
+def airline2(request):
+    return render(request, "airline2.html")
+
+def booked(request):
+    return render(request, "booked.html")
+
+def booking(request):
+    return render(request, "booking.html")
